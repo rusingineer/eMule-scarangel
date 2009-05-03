753e7c473c1a58aa42302e0a9a3b8d816a72f1f0
Stulle Ich <stulleamgym@gmx.net>
2009-05-03 20:20:33 +0200

    fix IDS definition

--- srchybrid/resource.h
+++ srchybrid/resource.h
@@ -3295,7 +3295,7 @@
 #define IDS_IPFILTERWHITELOADED         61410 // IP Filter White List [Stulle] - Stulle
 #define IDS_TRAY_COMPLETE               61420 // Completed in Tray [Stulle] - Stulle
 #define IDS_COLOR_FEEDBACK              61430 // Color Upload Feedback - MyTh88
-#define IDS_STATIC_ICON                 61440 // Static Tray Icon - MyTh88
+#define IDS_STATIC_ICON                 61450 // Static Tray Icon - MyTh88
 
 // Next default values for new objects
 // 
