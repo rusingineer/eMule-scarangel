8cba4170c8297cd7e03fcbc49399941dd81a80a4
Stulle Ich <stulleamgym@gmx.net>
2011-06-01 19:54:05 +0200

    Fixed: WebServer progress text did not work when file completed [Stulle] - Stulle

--- srchybrid/WebServer.cpp
+++ srchybrid/WebServer.cpp
@@ -5551,6 +5551,8 @@ CString CWebServer::_GetDownloadGraph(ThreadData Data, CString filehash)
 	/**/	_stprintf(HTTPHeader, _T("%.1f%%"), (static_cast<double>(pPartFile->GetPercentCompleted())));   
 	/**/	Out.Replace(_T("[FilePercentage]"), HTTPHeader);
 	/**/}
+	/**/else
+	/**/	Out.Replace(_T("[FilePercentage]"), _T("100%"));
 	// <== File Percentage in title of progress images in WebServer [unknown/Stulle] - Stulle
 	return Out;
 }
